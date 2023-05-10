6d07820803a92a6b1b511916eed591c7bf6263da
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-05-10 20:24:29 +0100

    Rebuild module

---
+++ npm_module/_dist/assets/svgs/16x16/Cross.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports["default"] = void 0;
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+var _react = _interopRequireDefault(require("react"));
+/* eslint no-unused-vars: [ "off", { "argsIgnorePattern": "tw" } ] */
+
+function CrossIcon() {
+  return /*#__PURE__*/_react["default"].createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "16",
+    height: "16",
+    fill: "none",
+    viewBox: "0 0 16 16"
+  }, /*#__PURE__*/_react["default"].createElement("path", {
+    fill: "#D9D9D9",
+    d: "M0 0H16V16H0z",
+    opacity: "0.01"
+  }), /*#__PURE__*/_react["default"].createElement(_StyledPath, {
+    d: "M9.258 7.981l4.658 4.658c.112.112.112.3 0 .45l-.827.826c-.15.113-.338.113-.45 0l-.79-.788-3.83-3.869-4.658 4.658c-.112.112-.3.112-.45 0l-.826-.827c-.113-.15-.113-.338 0-.45L6.742 7.98l-4.657-4.62c-.113-.112-.113-.3 0-.45l.826-.826c.15-.113.338-.113.45 0L8.02 6.742l4.62-4.657c.112-.113.3-.113.45 0l.826.826c.113.15.113.338 0 .45l-.788.79-3.869 3.83z"
+  }));
+}
+var _default = Copy;
+exports["default"] = _default;
+var _StyledPath = (0, _styledComponents["default"])("path").withConfig({
+  displayName: "Cross___StyledPath",
+  componentId: "sc-aordyq-0"
+})({
+  "fill": "currentColor"
+});
