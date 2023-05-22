e891036e37e24e896c21c39eb76bd661325ff669
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-05-22 21:02:38 +0100

    Update NPM module to 0.0.34

--- npm_module/_dist/components/spinner/index.js
+++ npm_module/_dist/components/spinner/index.js
@@ -3,13 +3,16 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports["default"] = exports.SpinnerBlock = void 0;
-var _taggedTemplateLiteralLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/taggedTemplateLiteralLoose"));
 var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
+var _taggedTemplateLiteralLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/taggedTemplateLiteralLoose"));
 var _react = require("@emotion/react");
 var _styled2 = _interopRequireDefault(require("@emotion/styled"));
 var _react2 = _interopRequireDefault(require("react"));
-var _templateObject;
 var _excluded = ["color", "radius", "style", "duration", "strokeWidth", "center"];
+var _templateObject;
+var SVG = _styled2["default"].svg(function () {
+  return [(0, _react.css)(_templateObject || (_templateObject = (0, _taggedTemplateLiteralLoose2["default"])(["\n    @keyframes rotate {\n      100% {\n        transform: rotate(360deg);\n      }\n    }\n    animation-name: rotate;\n    animation-timing-function: linear;\n    display: inline-block;\n    animation-iteration-count: infinite;\n  "])))];
+});
 function Spinner(_ref) {
   var color = _ref.color,
     radius = _ref.radius,
@@ -26,9 +29,8 @@ function Spinner(_ref) {
   var Path = getPathElement(color);
   return /*#__PURE__*/_react2["default"].createElement("span", {
     style: styles
-  }, /*#__PURE__*/_react2["default"].createElement("svg", Object.assign({}, props, {
+  }, /*#__PURE__*/_react2["default"].createElement(SVG, Object.assign({}, props, {
     viewBox: "0 0 42 42",
-    css: (0, _react.css)(_templateObject || (_templateObject = (0, _taggedTemplateLiteralLoose2["default"])(["\n          @keyframes rotate {\n            100% {\n              transform: rotate(360deg);\n            }\n          }\n          animation-name: rotate;\n          animation-timing-function: linear;\n          display: inline-block;\n          animation-iteration-count: infinite;\n        "]))),
     style: {
       animationDuration: (duration ? duration : 900) + "ms",
       width: (radius ? radius : 40) + "px",
