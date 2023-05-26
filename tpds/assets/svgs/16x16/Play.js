e42a291194b78d4dd050309b74a384a4a79bdb28
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-05-26 17:53:03 +0100

    Use SVG native currentColor instead of TW equivalent

--- tpds/assets/svgs/16x16/Play.js
+++ tpds/assets/svgs/16x16/Play.js
@@ -1,6 +1,4 @@
-/* eslint no-unused-vars: [ "off", { "argsIgnorePattern": "tw" } ] */
 import React from 'react'
-import tw from 'twin.macro'
 
 function Play({ ...props }) {
   return (
@@ -9,10 +7,13 @@ function Play({ ...props }) {
       xmlns="http://www.w3.org/2000/svg"
       width="11"
       height="13"
-      tw="fill-current text-primary"
+      fill="none"
       {...props}
     >
-      <path d="M9.938 5.028L1.688.153C1.007-.245 0 .153 0 1.114v9.75c0 .89.938 1.43 1.688.985l8.25-4.875a1.14 1.14 0 000-1.946z"></path>
+      <path
+        fill="currentColor"
+        d="M9.938 5.028L1.688.153C1.007-.245 0 .153 0 1.114v9.75c0 .89.938 1.43 1.688.985l8.25-4.875a1.14 1.14 0 000-1.946z"
+      ></path>
     </svg>
   )
 }
