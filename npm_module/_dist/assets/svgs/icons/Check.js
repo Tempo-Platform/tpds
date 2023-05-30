4cd24dcb0d278d026c886bf061467b4cf7c40b8e
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-05-30 18:29:14 +0100

    Update NPM module to 0.0.52

--- npm_module/_dist/assets/svgs/icons/Check.js
+++ npm_module/_dist/assets/svgs/icons/Check.js
@@ -14,11 +14,9 @@ function CheckIcon(_ref) {
     width: "11",
     height: "9",
     viewBox: "0 0 11 9",
-    css: {
-      "fill": "currentColor",
-      "color": "var(--text-primary)"
-    }
+    fill: "none"
   }, props), /*#__PURE__*/_react["default"].createElement("path", {
+    fill: "currentColor",
     d: "M9.563.088c.117-.117.304-.117.398 0l.68.656c.093.117.093.305 0 .399l-7.032 7.03a.27.27 0 01-.398 0L.07 5.058c-.094-.118-.094-.305 0-.399l.679-.68c.094-.093.281-.093.398 0l2.25 2.274L9.563.088z"
   }));
 }
--- tpds/assets/svgs/icons/Check.js
+++ tpds/assets/svgs/icons/Check.js
@@ -1,6 +1,5 @@
 /* eslint no-unused-vars: [ "off", { "argsIgnorePattern": "tw" } ] */
 import React from 'react'
-import tw from 'twin.macro'
 
 function CheckIcon({ ...props }) {
   return (
@@ -9,10 +8,13 @@ function CheckIcon({ ...props }) {
       width="11"
       height="9"
       viewBox="0 0 11 9"
-      tw="fill-current text-primary"
+      fill="none"
       {...props}
     >
-      <path d="M9.563.088c.117-.117.304-.117.398 0l.68.656c.093.117.093.305 0 .399l-7.032 7.03a.27.27 0 01-.398 0L.07 5.058c-.094-.118-.094-.305 0-.399l.679-.68c.094-.093.281-.093.398 0l2.25 2.274L9.563.088z"></path>
+      <path
+        fill="currentColor"
+        d="M9.563.088c.117-.117.304-.117.398 0l.68.656c.093.117.093.305 0 .399l-7.032 7.03a.27.27 0 01-.398 0L.07 5.058c-.094-.118-.094-.305 0-.399l.679-.68c.094-.093.281-.093.398 0l2.25 2.274L9.563.088z"
+      ></path>
     </svg>
   )
 }
