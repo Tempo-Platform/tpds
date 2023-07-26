6736c762538643108917327817b255c75f6dd1c2
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-07-26 18:11:35 +0100

    Add ThumbnailsRow component

---
+++ src/tpds/components/thumbnails-row/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import clsx from 'clsx'
+
+function ThumbnailsRow({ images, props }) {
+  const gridClassName = clsx(
+    `w-full h-auto rounded-lg p-2`,
+    `grid grid-cols-4 gap-2`,
+    `bg-grey-dark-scale-600/10`,
+  )
+  const iconClassName = clsx(
+    `flex items-center justify-center cursor-pointer`,
+    `!bg-grey-dark-scale-600/20 box-border`,
+    `w-full aspect-square`,
+    `rounded overflow-hidden`,
+  )
+  return (
+    <div className={gridClassName} {...props}>
+      {images.map(image => (
+        <div
+          key={image.url}
+          className={iconClassName}
+          onClick={image.callBack ? image.callBack : null}
+          style={{
+            background: `url(${image.url}) no-repeat center center`,
+            backgroundSize: 'contain',
+          }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default ThumbnailsRow
