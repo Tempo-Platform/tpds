c7a1236620954addad4379ff6d544faad16e2583
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-21 21:06:07 +0100

    Import newly created colors CSS file, update imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
 import '../lib/globals.css'
+import '../tpds/system/basestyles.css'
+import '../tpds/system/colors.css'
 import Layout from '../lib/layout'
-import Colors from '@/tpds/system/Colors'
 import Fonts from '@/tpds/system/Fonts'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Colors />
       <Component {...pageProps} />
     </Layout>
   )
