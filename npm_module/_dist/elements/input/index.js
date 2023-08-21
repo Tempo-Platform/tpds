ff41e47c17c0184dde1795515e894611fe424ecc
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-21 23:16:44 +0100

    Rebuild

--- npm_module/_dist/elements/input/index.js
+++ npm_module/_dist/elements/input/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.TextInput = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _clsx = _interopRequireDefault(require("clsx"));
-var _excluded = ["isValid", "isInvalid", "isWarning"];
+var _excluded = ["isValid", "isInvalid", "isWarning", "className"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
@@ -15,8 +15,9 @@ var TextInput = function TextInput(_ref) {
   var isValid = _ref.isValid,
     isInvalid = _ref.isInvalid,
     isWarning = _ref.isWarning,
+    className = _ref.className,
     props = _objectWithoutProperties(_ref, _excluded);
-  var finalClass = (0, _clsx["default"])(props.className, 'flex w-full', 'transition duration-100', 'px-2.5 py-1 rounded focus:outline-none', 'font-normal', 'bg-input text-primary border-input', 'border-2 focus:border-blue',
+  var finalClass = (0, _clsx["default"])(className, 'flex w-full', 'transition duration-100', 'px-2.5 py-1 rounded focus:outline-none', 'font-normal', 'bg-input text-primary border-input', 'border-2 focus:border-blue',
   //buttonVariantStyles[variant],
   isValid && 'border-green focus:border-green', isInvalid && 'border-red focus:border-red', isWarning && 'border-orange focus:border-orange');
   return /*#__PURE__*/_react["default"].createElement("input", _extends({
