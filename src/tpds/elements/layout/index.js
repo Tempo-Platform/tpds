7a49c17cc1a73a37daa550d6051bc5f9acdd5508
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-24 21:40:34 +0100

    Layout elements: allow passing regular style prop

--- src/tpds/elements/layout/index.js
+++ src/tpds/elements/layout/index.js
@@ -4,13 +4,21 @@ import clsx from 'clsx'
 export const Section = props => {
   const defaultClass = 'flex flex-col w-full'
   const finalClass = clsx(defaultClass, props.className)
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 export const PageRoot = props => {
   const defaultClass = 'flex flex-col w-full min-h-screen'
   const finalClass = clsx(defaultClass, props.className)
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 const containerVariants = {
@@ -23,37 +31,61 @@ export const Container = props => {
   const defaultClass = 'self-center mx-auto'
   const variant = props.variant || 'large'
   const finalClass = clsx(defaultClass, props.className, containerVariants[variant])
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 export const ContainerRow = props => {
   const defaultClass = 'self-center flex-row'
   const variant = props.variant || 'large'
   const finalClass = clsx(defaultClass, props.className, containerVariants[variant])
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 export const PaddingBox = props => {
   const defaultClass = 'py-4 md:py-8 lg:py-12 xl:py-20'
   const finalClass = clsx(defaultClass, props.className)
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 export const Separator = props => {
   const defaultClass = 'h-[1px] w-full mt-[20px] border-b border-body'
   const finalClass = clsx(defaultClass, props.className)
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 export const SeparatorWindow = props => {
   const defaultClass = 'h-[1px] w-full mt-[20px] border-b border-window'
   const finalClass = clsx(defaultClass, props.className)
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
 
 export const Window = props => {
   const defaultClass =
     'bg-window border border-window rounded-sm py-xs px-xs md:py-xs md:px-sm lg:py-sm lg:px-md lg:rounded'
   const finalClass = clsx(defaultClass, props.className)
-  return <div className={finalClass}>{props.children}</div>
+  return (
+    <div className={finalClass} style={props.style}>
+      {props.children}
+    </div>
+  )
 }
