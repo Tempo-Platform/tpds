9bc2854d684ff8c09206aef78355a54f7e44704f
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-24 21:41:43 +0100

    Rebuild and publish

--- npm_module/_dist/elements/layout/index.js
+++ npm_module/_dist/elements/layout/index.js
@@ -11,7 +11,8 @@ var Section = function Section(props) {
   var defaultClass = 'flex flex-col w-full';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.Section = Section;
@@ -19,7 +20,8 @@ var PageRoot = function PageRoot(props) {
   var defaultClass = 'flex flex-col w-full min-h-screen';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.PageRoot = PageRoot;
@@ -33,7 +35,8 @@ var Container = function Container(props) {
   var variant = props.variant || 'large';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className, containerVariants[variant]);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.Container = Container;
@@ -42,7 +45,8 @@ var ContainerRow = function ContainerRow(props) {
   var variant = props.variant || 'large';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className, containerVariants[variant]);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.ContainerRow = ContainerRow;
@@ -50,7 +54,8 @@ var PaddingBox = function PaddingBox(props) {
   var defaultClass = 'py-4 md:py-8 lg:py-12 xl:py-20';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.PaddingBox = PaddingBox;
@@ -58,7 +63,8 @@ var Separator = function Separator(props) {
   var defaultClass = 'h-[1px] w-full mt-[20px] border-b border-body';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.Separator = Separator;
@@ -66,7 +72,8 @@ var SeparatorWindow = function SeparatorWindow(props) {
   var defaultClass = 'h-[1px] w-full mt-[20px] border-b border-window';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.SeparatorWindow = SeparatorWindow;
@@ -74,7 +81,8 @@ var Window = function Window(props) {
   var defaultClass = 'bg-window border border-window rounded-sm py-xs px-xs md:py-xs md:px-sm lg:py-sm lg:px-md lg:rounded';
   var finalClass = (0, _clsx["default"])(defaultClass, props.className);
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: finalClass
+    className: finalClass,
+    style: props.style
   }, props.children);
 };
 exports.Window = Window;
