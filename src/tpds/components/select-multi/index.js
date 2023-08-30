5814bdc503c29b4cc5cd80e17f13181aa68a6e3b
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-30 19:28:08 +0100

    MultiSelect: add arrow icon, improve click&focus behaviour

--- src/tpds/components/select-multi/index.js
+++ src/tpds/components/select-multi/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
 import clsx from 'clsx'
-import { TextInput } from '../../elements/input'
 import { PTiny } from '../../elements/typography'
 import Tag from '../tag'
 
@@ -25,8 +24,10 @@ const Select = ({
   excludeIndexes = [],
   labelProp = 'value',
   placeholder = 'Select',
+  tagVariant = 'secondary',
 }) => {
   const wrapperRef = useRef(null)
+  const inputRef = useRef(null)
   const optionsWithoutExcludedIndexes = options.filter(
     (option, index) => !excludeIndexes.includes(index),
   )
@@ -86,65 +87,81 @@ const Select = ({
   )
 
   return (
-    <div className="w-full relative" ref={wrapperRef}>
-      {
-        <div
-          className={clsx(
-            'flex flex-row flex-wrap gap-2 items-center justify-between',
-            'w-full',
-            'px-2.5 py-1 rounded focus:outline-none',
-            'font-normal',
-            'bg-grey-light-scale-200 text-primary border-transparent dark:bg-grey-dark-scale-400',
-            'border-2 focus:border-blue',
-            'cursor-pointer',
-            'select-none',
-            'transition duration-100',
-            isOpen && 'border-blue',
-          )}
-        >
-          <div className="flex gap-2 flex-wrap">
-            {displayValue &&
-              displayValue.map((item, index) => (
-                <Tag
-                  className="inline-flex"
-                  variant="warning"
-                  key={index}
-                  label={item[labelProp] || item}
-                  showCloseIcon={true}
-                  addHoverEffect={true}
-                  onClick={() => {
-                    removeSelectedItem(item)
-                  }}
-                />
-              ))}
-            <input
-              className={clsx(
-                'inline-flex',
-                'bg-transparent',
-                'font-normal',
-                'w-auto',
-                'p-0',
-                '!outline-none',
-                '!border-none',
-              )}
-              type="text"
-              placeholder={placeholder}
-              value={inputValueToDisplay}
-              onChange={e => setInputValue(e.target.value)}
-              onFocus={() => {
-                setInputValue('')
-                setIsOpen(true)
-              }}
-            />
-          </div>
+    <div className="w-full relative" ref={wrapperRef} onClick={() => inputRef.current.focus()}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={clsx(
+          'cursor-pointer',
+          'w-4 h-4 absolute right-2 transform top-[10px] text-primary',
+          isOpen && 'rotate-180',
+        )}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+      </svg>
+      <div
+        className={clsx(
+          'flex flex-row flex-wrap gap-2 items-center justify-between',
+          'w-full',
+          'px-2.5 py-1 rounded focus:outline-none',
+          'font-normal',
+          'bg-grey-light-scale-200 text-primary border-transparent dark:bg-grey-dark-scale-400',
+          'border-2 focus:border-blue',
+          'cursor-pointer',
+          'select-none',
+          'transition duration-100',
+          isOpen && 'border-blue',
+        )}
+      >
+        <div className="flex gap-2 flex-wrap">
+          {displayValue &&
+            displayValue.map((item, index) => (
+              <Tag
+                className="inline-flex"
+                variant={tagVariant}
+                key={index}
+                label={item[labelProp] || item}
+                showCloseIcon={true}
+                addHoverEffect={true}
+                onClick={() => {
+                  removeSelectedItem(item)
+                }}
+              />
+            ))}
+          <input
+            ref={inputRef}
+            className={clsx(
+              'inline-flex',
+              'bg-transparent',
+              'font-normal',
+              'w-auto',
+              'p-0',
+              'mr-auto',
+              '!outline-none',
+              '!border-none',
+            )}
+            type="text"
+            placeholder={placeholder}
+            value={inputValueToDisplay}
+            onChange={e => setInputValue(e.target.value)}
+            onFocus={() => {
+              setInputValue('')
+              setIsOpen(true)
+            }}
+          />
         </div>
-      }
+      </div>
       {isOpen && (
         <div className="w-full flex flex-col space-y-1 items-start text-start p-2 rounded bg-window border-2 border-window z-50 absolute top-[100%] left-0 max-h-40 overflow-auto">
           {optionsThatAreStillNotSelected.map((option, index) => (
             <div
               key={labelProp ? option[labelProp] : option}
-              onClick={() => {
+              onClick={e => {
+                e.stopPropagation()
+                e.preventDefault()
                 addSelectedItem(option)
                 setIsOpen(false)
               }}
