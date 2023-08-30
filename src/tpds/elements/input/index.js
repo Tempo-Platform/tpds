e21eadaadf993d617f99a0ba845e8abd194496e2
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-30 03:38:24 +0100

    Export standard input styles

--- src/tpds/elements/input/index.js
+++ src/tpds/elements/input/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import clsx from 'clsx'
 
+export const baseInputStyles = [
+  'flex w-full',
+  'transition duration-100',
+  'px-2.5 py-1 rounded focus:outline-none',
+  'font-normal',
+  'bg-grey-light-scale-200 text-primary border-transparent dark:bg-grey-dark-scale-300',
+  'border-2 focus:border-blue',
+]
+
 export const TextInput = ({ isValid, isInvalid, isWarning, className, ...props }) => {
   const finalClass = clsx(
     className,
-    'flex w-full',
-    'transition duration-100',
-    'px-2.5 py-1 rounded focus:outline-none',
-    'font-normal',
-    'bg-grey-light-scale-200 text-primary border-transparent dark:bg-grey-dark-scale-300',
-    'border-2 focus:border-blue',
+    ...baseInputStyles,
     isValid && 'border-green focus:border-green',
     isInvalid && 'border-red focus:border-red',
     isWarning && 'border-orange focus:border-orange',
