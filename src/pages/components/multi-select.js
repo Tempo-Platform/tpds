65aa4c3f6391881f2e710c0b14638935037c875b
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-30 19:28:44 +0100

    Select & MultiSelect: tidy up doc pages

--- src/pages/components/multi-select.js
+++ src/pages/components/multi-select.js
@@ -33,6 +33,7 @@ export default function Page() {
             selectedIndexes={selectedIndexes}
             labelProp="label"
             idProp="value"
+            tagVariant="primary"
             excludeIndexes={[2]}
             options={[
               { value: '1', label: 'Option 1' },
--- src/pages/components/select.js
+++ src/pages/components/select.js
@@ -32,23 +32,22 @@ export default function Page() {
           <br />
           <Select
             handleIndexSelection={setSelectedIndex}
-            selectedIndex={null}
-            labelProp={null}
+            selectedIndex={selectedIndex}
+            labelProp={'label'}
             idProp="value"
             excludeIndexes={[2]}
             allowMultiple={true}
-            noPermanentSelection={true}
             options={[
-              'Option 1',
-              'Option 2',
-              'Option 3',
-              'Option 4',
-              'Option 5',
-              // { value: '1', label: 'Option 1' },
-              // { value: '2', label: 'Option 2' },
-              // { value: '3', label: 'Option 3' },
-              // { value: '4', label: 'Option 4' },
-              // { value: '5', label: 'Option 5' },
+              // 'Option 1',
+              // 'Option 2',
+              // 'Option 3',
+              // 'Option 4',
+              // 'Option 5',
+              { value: '1', label: 'Option 1' },
+              { value: '2', label: 'Option 2' },
+              { value: '3', label: 'Option 3' },
+              { value: '4', label: 'Option 4' },
+              { value: '5', label: 'Option 5' },
               // { value: '6', label: 'Option 6' },
               // { value: '7', label: 'Option 7' },
               // { value: '8', label: 'Option 8' },
