556625a3392827e926fddc17fc8ba73fa5be64e5
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-08-31 22:00:52 +0100

    Adjust background color

--- src/tpds/components/radiotabs/index.js
+++ src/tpds/components/radiotabs/index.js
@@ -4,7 +4,7 @@ import clsx from 'clsx'
 const RadioTabs = ({ items, selectedIndex, handleIndexSelection }) => {
   return (
     <div className="w-full">
-      <div className="flex space-x-1 rounded-lg p-1 bg-grey-light-scale-300 dark:bg-grey-dark-scale-500">
+      <div className="flex space-x-1 rounded-lg p-1 bg-grey-light-scale-200 dark:bg-grey-dark-scale-300">
         {items.map((item, index) => (
           <div
             className="flex-1"
@@ -17,8 +17,7 @@ const RadioTabs = ({ items, selectedIndex, handleIndexSelection }) => {
                 'text-xs xl:text-sm',
                 '!outline-none',
                 `w-full select-none cursor-pointer text-center py-2 rounded-md`,
-                index === selectedIndex &&
-                  `bg-grey-light-scale-100 text-black dark:bg-grey-dark-scale-300`,
+                index === selectedIndex && `bg-white text-black dark:bg-grey-dark-scale-200`,
               )}
             >
               {item.value || item.description || item.label || item}
