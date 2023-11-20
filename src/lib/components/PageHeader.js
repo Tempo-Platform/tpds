4049cb6dde11a86f2720d98617297297472e8db9
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-11-20 17:55:53 +0000

    Docs: introduce PageHeader component, to use instead of standard header across docs pages

---
+++ src/lib/components/PageHeader.js
@@ -0,0 +1,17 @@
+import { P, H4 } from '../../tpds/elements/typography'
+import { Separator } from '../../tpds/elements/layout'
+
+const PageHeader = ({ title, subTitle }) => {
+  if (!title) {
+    throw new Error('PageHeader requires a title prop')
+  }
+  return (
+    <div className="mt-20 mb-10 lg:mt-20 lg:mb-12">
+      <H4 isBold>{title}</H4>
+      {subTitle && <P className="my-0">{subTitle}</P>}
+      <Separator className="mt-10" />
+    </div>
+  )
+}
+
+export default PageHeader
