01d6ace376502410869d0fd2b92e830bf9a991f7
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-11-24 19:28:28 +0000

    Update NPM module to 1.2.53

--- npm_module/_dist/components/callout/index.js
+++ npm_module/_dist/components/callout/index.js
@@ -13,6 +13,7 @@ var _InfoIcon = _interopRequireDefault(require("../../assets/svgs/notifications/
 var _TipIcon = _interopRequireDefault(require("../../assets/svgs/notifications/TipIcon"));
 var _BugIcon = _interopRequireDefault(require("../../assets/svgs/notifications/BugIcon"));
 var _NoteIcon = _interopRequireDefault(require("../../assets/svgs/notifications/NoteIcon"));
+var _typography = require("@/tpds/elements/typography");
 var _excluded = ["variant", "title", "text", "children"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -22,22 +23,22 @@ var rootStyles = {
   "default": 'border-grey-light-scale-400 dark:border-grey-dark-scale-200 bg-grey-light-scale-200 dark:bg-grey-light-scale-100/5',
   success: 'border-green/50 bg-green-scale-100/20 dark:bg-green-scale-500/10',
   info: 'border-blue/50 bg-blue-scale-100/20 dark:bg-blue-scale-300/10',
-  danger: 'border-red-scale-100 bg-red-scale-100/20 dark:border-red-scale-300 dark:bg-red-scale-300/10',
+  danger: 'border-red/50 bg-red-scale-100/20 dark:bg-red-scale-300/10',
   warning: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/10',
   tip: 'border-purple/50 bg-purple-scale-100/20 dark:bg-purple-scale-300/10',
   bug: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/10',
-  note: 'border-grey-light-scale-400 dark:border-grey-dark-scale-200 dark:bg-grey-light-scale-100/5'
+  note: 'border-grey-light-scale-400 dark:border-grey-dark-scale-200 bg-grey-light-scale-200 dark:bg-grey-light-scale-100/5'
 };
 var headerBaseStyle = 'flex items-center font-medium text-lg pt-4 px-4';
 var headerVariantStyles = {
-  "default": 'text-grey-dark-scale-700 dark:text-grey-light-scale-500',
-  success: 'text-green-scale-500 dark:text-green-scale-400',
-  info: 'text-blue-scale-500 dark:text-blue-scale-300',
-  danger: 'text-red-scale-500 dark:text-red-scale-400',
-  warning: 'text-orange-scale-500 dark:text-orange-scale-400',
-  tip: 'text-purple-scale-500 dark:text-purple-scale-100',
-  bug: 'text-orange-scale-500 dark:text-orange-scale-400',
-  note: 'text-grey-dark-scale-700 dark:text-white'
+  "default": '!text-grey-dark-scale-700 dark:!text-grey-light-scale-500',
+  success: '!text-green-scale-500 dark:!text-green-scale-400',
+  info: '!text-blue-scale-500 dark:!text-blue-scale-300',
+  danger: '!text-red-scale-500 dark:!text-red-scale-400',
+  warning: '!text-orange-scale-500 dark:!text-orange-scale-400',
+  tip: '!text-purple-scale-500 dark:!text-purple-scale-100',
+  bug: '!text-orange-scale-500 dark:!text-orange-scale-400',
+  note: '!text-grey-dark-scale-700 dark:!text-white'
 };
 var getIcon = function getIcon(variant) {
   switch (variant) {
@@ -69,12 +70,17 @@ var Callout = function Callout(_ref) {
   return /*#__PURE__*/_react["default"].createElement("div", _extends({}, props, {
     className: (0, _clsx["default"])('border rounded-lg', rootStyles[variant])
   }), /*#__PURE__*/_react["default"].createElement("div", {
-    className: (0, _clsx["default"])(headerBaseStyle, headerVariantStyles[variant])
+    className: (0, _clsx["default"])(headerBaseStyle)
   }, variant !== 'default' && /*#__PURE__*/_react["default"].createElement("span", {
     className: "mr-2"
-  }, getIcon(variant)), title), /*#__PURE__*/_react["default"].createElement("div", {
-    className: "font-medium text-grey-dark-scale-200 dark:text-white px-4 pb-4 pt-2"
-  }, text, children));
+  }, getIcon(variant)), title && /*#__PURE__*/_react["default"].createElement(_typography.P, {
+    isMedium: true,
+    className: (0, _clsx["default"])(headerVariantStyles[variant])
+  }, title)), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "px-4 pb-4 pt-2"
+  }, text && /*#__PURE__*/_react["default"].createElement(_typography.PSmall, {
+    isMedium: true
+  }, text), children));
 };
 var _default = Callout;
 exports["default"] = _default;
