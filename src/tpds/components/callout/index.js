2cdd263ef249d5b6f06aaa04f4ea129f43dd0c86
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-11-29 20:32:57 +0000

    Callout component: Adjust style

--- src/tpds/components/callout/index.js
+++ src/tpds/components/callout/index.js
@@ -12,12 +12,12 @@ import { P, PSmall } from '../../elements/typography'
 const rootStyles = {
   default:
     'border-grey-light-scale-400 dark:border-grey-dark-scale-200 bg-grey-light-scale-200 dark:bg-grey-light-scale-100/5',
-  success: 'border-green/50 bg-green-scale-100/20 dark:bg-green-scale-500/10',
-  info: 'border-blue/50 bg-blue-scale-100/20 dark:bg-blue-scale-300/10',
-  danger: 'border-red/50 bg-red-scale-100/20 dark:bg-red-scale-300/10',
-  warning: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/10',
-  tip: 'border-purple/50 bg-purple-scale-100/20 dark:bg-purple-scale-300/10',
-  bug: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/10',
+  success: 'border-green/50 bg-green-scale-100/20 dark:bg-green-scale-500/5',
+  info: 'border-blue/50 bg-blue-scale-100/20 dark:bg-blue-scale-300/5',
+  danger: 'border-red/50 bg-red-scale-100/20 dark:bg-red-scale-300/5',
+  warning: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/5',
+  tip: 'border-purple/50 bg-purple-scale-100/20 dark:bg-purple-scale-300/5',
+  bug: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/5',
   note: 'border-grey-light-scale-400 dark:border-grey-dark-scale-200 bg-grey-light-scale-200 dark:bg-grey-light-scale-100/5',
 }
 
@@ -25,12 +25,12 @@ const headerBaseStyle = 'flex items-center font-medium text-lg pt-4 px-4'
 
 const headerVariantStyles = {
   default: '!text-grey-dark-scale-700 dark:!text-grey-light-scale-500',
-  success: '!text-green-scale-500 dark:!text-green-scale-400',
-  info: '!text-blue-scale-500 dark:!text-blue-scale-300',
-  danger: '!text-red-scale-500 dark:!text-red-scale-400',
-  warning: '!text-orange-scale-500 dark:!text-orange-scale-400',
-  tip: '!text-purple-scale-500 dark:!text-purple-scale-100',
-  bug: '!text-orange-scale-500 dark:!text-orange-scale-400',
+  success: '!text-green dark:!text-green',
+  info: '!text-blue dark:!text-blue',
+  danger: '!text-red dark:!text-red',
+  warning: '!text-orange dark:!text-orange',
+  tip: '!text-purple dark:!text-purple',
+  bug: '!text-orange dark:!text-orange',
   note: '!text-grey-dark-scale-700 dark:!text-white',
 }
 
