4820ba51223438c2e43696f2c7fefa10831781f4
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-12-14 21:32:09 +0000

    TextInput elemnet: fix output style hover states

--- src/tpds/elements/input/index.js
+++ src/tpds/elements/input/index.js
@@ -6,12 +6,14 @@ export const baseInputStyles = [
   'transition duration-100',
   'px-2.5 py-1 rounded focus:outline-none',
   'font-normal',
-  'bg-input text-primary border-transparent',
+  'bg-input',
+  'border-transparent',
+  'text-primary border-transparent',
   'border-2 focus:border-blue',
 ]
 
 export const outlineStyles = [
-  'bg-transparent border-[#ededed] dark:border-[#384147] dark:focus:border-blue',
+  'bg-transparent !border-[#ededed] focus:!border-blue dark:!border-[#384147] dark:focus:!border-blue',
 ]
 
 export const TextInput = ({ isValid, isInvalid, isWarning, outlineStyle, className, ...props }) => {
@@ -19,9 +21,10 @@ export const TextInput = ({ isValid, isInvalid, isWarning, outlineStyle, classNa
     ...baseInputStyles,
     outlineStyle && outlineStyles,
     className,
-    isValid && '!border-green focus:!border-green',
-    isInvalid && '!border-red focus:!border-red',
-    isWarning && '!border-orange focus:!border-orange',
+    isValid && '!border-green focus:!border-green dark:!border-green dark:focus:!border-green',
+    isInvalid && '!border-red focus:!border-red dark:!border-red dark:focus:!border-red',
+    isWarning &&
+      '!border-orange focus:!border-orange dark:!border-orange dark:focus:!border-orange',
   )
   return <input type="text" className={finalClass} {...props} />
 }
