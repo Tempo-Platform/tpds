fbccc4496f8419dd7a1409617641584e5fcb0e3a
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-12-29 19:25:58 +0000

    Update NPM module to 1.2.76

--- npm_module/_dist/components/thumbnails-island/index.js
+++ npm_module/_dist/components/thumbnails-island/index.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _clsx = _interopRequireDefault(require("clsx"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-function ThumbnailsRow(_ref) {
+function ThumbnailsIsland(_ref) {
   var _ref$items = _ref.items,
     items = _ref$items === void 0 ? null : _ref$items,
     _ref$backgroundColor = _ref.backgroundColor,
@@ -24,7 +24,7 @@ function ThumbnailsRow(_ref) {
     thumbnailClassName = _ref.thumbnailClassName,
     rootClassName = _ref.rootClassName;
   if (!items) {
-    console.error('ThumbnailsRow: items prop is required');
+    console.error('ThumbnailsIsland: items prop is required');
     return null;
   }
   var styles1Items = 'grid-cols-1 gap-3 p-2';
@@ -32,7 +32,7 @@ function ThumbnailsRow(_ref) {
   var styles3Items = 'grid-cols-3 gap-1.5 p-2';
   var styles4Items = 'grid-cols-4 gap-2 p-2';
   var styles5Items = 'grid-cols-5 gap-2 p-2';
-  var styles6Items = 'grid-cols-6 gap-1.5 p-2';
+  var styles6Items = 'grid-cols-6 gap-1 p-1';
   var gridClassName = (0, _clsx["default"])("grid w-full h-auto rounded-lg", islandClassName, items.length === 1 && styles1Items, items.length === 2 && styles2Items, items.length === 3 && styles3Items, items.length === 4 && styles4Items, items.length === 5 && styles5Items, items.length === 6 && styles6Items, removeEnclosingIsland && '!p-0');
   var iconClassName = (0, _clsx["default"])("box-border", "flex items-center justify-center cursor-pointer", "rounded overflow-hidden", "!aspect-square", thumbnailClassName);
   if (backgroundColor.indexOf('#') === -1) {
@@ -74,5 +74,5 @@ function ThumbnailsRow(_ref) {
     }));
   })));
 }
-var _default = ThumbnailsRow;
+var _default = ThumbnailsIsland;
 exports["default"] = _default;
