c722cfec1d940a2ad5d1884baeebf83b4f4c506d
Antonio Brandao <antoniobrandaodesign@gmail.com>
2024-02-08 19:41:20 +0000

    Rework RadioTabs styles

--- src/tpds/components/radiotabs/index.js
+++ src/tpds/components/radiotabs/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import clsx from 'clsx'
 
-const RadioTabs = ({ items, selectedIndex, handleIndexSelection, outlineStyle }) => {
+const RadioTabs = ({
+  items,
+  selectedIndex,
+  handleIndexSelection,
+  outlineStyle,
+  className,
+  style,
+}) => {
   return (
-    <div className="w-full">
+    <div className={clsx('w-full', className)} style={{ style }}>
       <div
         className={clsx(
-          'flex space-x-1 rounded-[8px] p-1',
+          'flex gap-x-[2px] rounded-[8px] p-[2px] h-[36px]',
           outlineStyle
             ? 'bg-transparent border !border-[#ededed] dark:!border-[#384147]'
             : 'bg-grey-light-scale-200 dark:bg-grey-dark-scale-500',
@@ -14,20 +21,24 @@ const RadioTabs = ({ items, selectedIndex, handleIndexSelection, outlineStyle })
       >
         {items.map((item, index) => (
           <div
-            className="flex-1"
+            className={clsx(
+              'flex-1 overflow-hidden',
+              '!px-[7px] py-[4px]',
+              'flex justify-center',
+              'select-none !outline-none',
+              index === selectedIndex && 'bg-black dark:text-black rounded-[5px]',
+            )}
             key={item.value || item.description || item.label || item}
             onClick={() => handleIndexSelection(index)}
           >
             <p
               className={clsx(
                 index === selectedIndex ? 'text-primary' : 'text-secondary',
-                'm-0 font-medium',
-                'px-4',
-                'text-xs xl:text-sm',
+                'font-medium',
+                'text-[14px]',
                 '!outline-none',
-                `w-full select-none cursor-pointer text-center py-2 rounded-[5px]`,
-                index === selectedIndex &&
-                  'bg-grey-dark-scale-500 text-white dark:bg-white dark:text-black',
+                'overflow-hidden whitespace-nowrap text-ellipsis',
+                index === selectedIndex && 'text-white dark:bg-white',
               )}
             >
               {item.value || item.description || item.label || item}
