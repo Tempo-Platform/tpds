b0f7962de1b9df6997f754db4fd04efa7ef90a2a
Antonio Brandao <antoniobrandaodesign@gmail.com>
2024-12-05 01:03:43 +0000

    Fix notificaiton icons color

--- npm_module/dist/assets/svgs/notifications/index.d.ts
+++ npm_module/dist/assets/svgs/notifications/index.d.ts
@@ -1,8 +1,14 @@
 import React from 'react';
-export declare function BugIcon(): React.JSX.Element;
-export declare function CheckIcon(): React.JSX.Element;
-export declare function ErrorIcon(): React.JSX.Element;
-export declare function InfoIcon(): React.JSX.Element;
-export declare function NoteIcon(): React.JSX.Element;
-export declare function TipIcon(): React.JSX.Element;
-export declare function WarningIcon(): React.JSX.Element;
+type IconProps = {
+    className?: string;
+    style?: React.CSSProperties;
+    onClick?: (e: React.MouseEvent) => void;
+};
+export declare function BugIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export declare function CheckIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export declare function ErrorIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export declare function InfoIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export declare function NoteIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export declare function TipIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export declare function WarningIcon({ className, style, onClick }: IconProps): React.JSX.Element;
+export {};
