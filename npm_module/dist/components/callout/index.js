b894355bc6c7573ca5165680eccf17bcd99bf4ea
Antonio Brandao <antoniobrandaodesign@gmail.com>
2024-12-12 22:16:58 +0000

    Rebuild and publish

--- npm_module/dist/components/callout/index.js
+++ npm_module/dist/components/callout/index.js
@@ -6,17 +6,8 @@ var clsx = require('clsx');
 var assets_svgs_notifications_index = require('../../assets/svgs/notifications/index.js');
 var elements_typography_index = require('../../elements/typography/index.js');
 
-const rootStyles = {
-    default: 'border-grey-light-scale-400 dark:border-grey-dark-scale-200 bg-grey-light-scale-200 dark:bg-grey-light-scale-100/5',
-    success: 'border-green/50 bg-green-scale-100/20 dark:bg-green-scale-500/5',
-    info: 'border-blue/50 bg-blue-scale-100/20 dark:bg-blue-scale-300/5',
-    danger: 'border-red/50 bg-red-scale-100/20 dark:bg-red-scale-300/5',
-    warning: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/5',
-    tip: 'border-purple/50 bg-purple-scale-100/20 dark:bg-purple-scale-300/5',
-    bug: 'border-orange/50 bg-orange-scale-100/20 dark:bg-orange-scale-300/5',
-    note: 'border-grey-light-scale-400 dark:border-grey-dark-scale-200 bg-grey-light-scale-200 dark:bg-grey-light-scale-100/5',
-};
-const headerBaseStyle = 'flex items-center font-medium text-lg pt-4 px-4';
+const baseStyle = 'bg-zinc-100 dark:bg-zinc-800 border-zinc-600 rounded-lg py-4 px-6';
+const headerBaseStyle = 'flex items-center font-medium text-lg';
 const headerVariantStyles = {
     default: '!text-grey-dark-scale-700 dark:!text-grey-light-scale-500',
     success: '!text-green dark:!text-green',
@@ -49,7 +40,7 @@ const getIcon = (variant) => {
 };
 const Callout = (_a) => {
     var { variant = 'default', title, text, omitIcon = false, customIcon, children } = _a, props = tslib_es6.__rest(_a, ["variant", "title", "text", "omitIcon", "customIcon", "children"]);
-    return (/*#__PURE__*/React.createElement("div", Object.assign({}, props, { className: clsx('border rounded-lg', rootStyles[variant]) }), "\n      ", /*#__PURE__*/React.createElement("div", { className: clsx(headerBaseStyle) }, "\n        ", !customIcon && !omitIcon && variant !== 'default' && /*#__PURE__*/React.createElement("span", { className: 'mr-2' }, getIcon(variant)), "\n        ", customIcon && !omitIcon && /*#__PURE__*/React.createElement("span", { className: 'mr-2' }, customIcon), "\n        ", title && (/*#__PURE__*/React.createElement(elements_typography_index.P, { isMedium: true, className: clsx(headerVariantStyles[variant]) }, "\n            ", title, "\n          ")), "\n      "), "\n      ", /*#__PURE__*/React.createElement("div", { className: 'px-4 pb-4 pt-2' }, "\n        ", text && /*#__PURE__*/React.createElement(elements_typography_index.PSmall, { isMedium: true }, text), "\n        ", children, "\n      "), "\n    "));
+    return (/*#__PURE__*/React.createElement("div", Object.assign({}, props, { className: baseStyle }), "\n      ", /*#__PURE__*/React.createElement("div", { className: clsx(headerBaseStyle) }, "\n        ", !customIcon && !omitIcon && variant !== 'default' && /*#__PURE__*/React.createElement("span", { className: 'mr-2' }, getIcon(variant)), "\n        ", customIcon && !omitIcon && /*#__PURE__*/React.createElement("span", { className: 'mr-2' }, customIcon), "\n        ", title && (/*#__PURE__*/React.createElement(elements_typography_index.P, { isMedium: true, className: clsx(headerVariantStyles[variant]) }, "\n            ", title, "\n          ")), "\n      "), "\n      ", /*#__PURE__*/React.createElement("div", { className: 'mt-2' }, "\n        ", text && /*#__PURE__*/React.createElement(elements_typography_index.PSmall, { isMedium: true, className: '!text-zinc-500 dark:!text-zinc-400' }, text), "\n        ", children, "\n      "), "\n    "));
 };
 
 module.exports = Callout;
